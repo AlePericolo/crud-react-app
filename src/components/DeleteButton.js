708422ef76a1b268891fb11e8f3e64f676ffc28c
alessandro <ale.pericolo@gmail.com>
2019-11-14 00:53:19 +0100

    add delete button component

---
+++ src/components/DeleteButton.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Service from './Service';
+import { Link } from 'react-router-dom';
+
+export default class DeleteButton extends React.Component {
+
+    deleteDocument = () => {
+
+        Service.deleteApi('/' + this.props.id.toString())
+            .then(response => {
+                console.log(response);
+                if (response.status === 200) {
+                    alert('Car deleted successfully!!');
+                    this.props.history.push('/index')
+                }
+                else {
+                    alert('something went wrong!!');
+                    this.props.history.push('/index')
+                }
+            }).catch((error) => {
+                console.log("error-----------", error)
+            })
+    }
+
+    render() {
+        return (
+            <button type="button" onClick={this.deleteDocument} className="btn btn-danger">Delete</button>
+        );
+    }
+
+}
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,62 +1,62 @@
 import React from 'react';
+import DeleteButton from './DeleteButton';
+
 
 export default class Table extends React.Component {
- 
-    constructor(props){
-        super(props);
-        console.log(this.props);
-    }
-    
-    getKeys = function(){
+
+    getKeys = function () {
         return Object.keys(this.props.data[0]);
     }
-    
-    getHeader = function(){
+
+    getHeader = function () {
         var keys = this.getKeys();
-        return keys.map((key, index)=>{
+        return keys.map((key, index) => {
             return <th key={key}>{key.toUpperCase()}</th>
         })
     }
-    
-    getRowsData = function(){
+
+    getRowsData = function () {
         var items = this.props.data;
         var keys = this.getKeys();
-        return items.map((row, index)=>{
-            return <tr key={index}><RenderRow key={index} data={row} keys={keys}/></tr>
+        return items.map((obj, index) => {
+            return (
+                <tr key={index}>
+                    <RenderRow key={index} data={obj} keys={keys} />
+                    <td>
+                        <DeleteButton id={obj.id} />
+                    </td>
+                </tr>
+            )
         })
     }
-    
+
     render() {
-    return (
-        <div>
-            <table className="table table-striped">
-                <thead>
-                    <tr>
-                        {this.getHeader()}
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.getRowsData()}
-                </tbody>
-            </table>
-    </div>
-    
-    );
+        return (
+            <div>
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            {this.getHeader()}
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.getRowsData()}
+                    </tbody>
+                </table>
+            </div>
+
+        );
     }
-   }
-   const RenderRow = (props) => {
-    return props.keys.map((key, index)=>{
-        console.log(props.keys.length);
-        console.log(index);
-        if(props.data[key] instanceof Object){
-            console.log(props.data[key]);
+}
+
+const RenderRow = (props) => {
+    return props.keys.map((key, index) => {
+        if (props.data[key] instanceof Object) {
+            //console.log(props.data[key]);
             return <td key={props.data[key].toString()}> inserire button che apre modale quality </td>
-        }else{
+        } else {
             return <td key={props.data[key].toString()}>{props.data[key].toString()}</td>
         }
-        if(index === props.keys.length){
-            return <td>a</td>
-        }
     })
-   }
+}
