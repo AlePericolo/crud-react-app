d4168f578d6e9a4829d07c2dbcd94b405c5be711
alessandro <alessandro.pericolo@dscsrl.it>
2019-11-15 17:38:53 +0100

    check delete response

--- src/components/container/DeletePage.js
+++ src/components/container/DeletePage.js
@@ -1,40 +1,58 @@
-import React, { Component } from 'react';
+import React, {
+    Component
+} from 'react';
 import Service from '../Service';
 import Title from '../common/Title'
 import Swal from 'sweetalert2'
-import { Link } from 'react-router-dom'
+import {
+    Link
+} from 'react-router-dom'
 
 class DeletePage extends Component {
 
     constructor(props) {
         super(props);
-    
-        this.state = { data: '', loadComplete: false };
+
+        this.state = {
+            data: '',
+            loadComplete: false
+        };
         this.addService = new Service();
     }
 
     componentDidMount() {
-        
-        const { params } = this.props.match
+
+        const {
+            params
+        } = this.props.match
         const p = `/${params.id}`;
-        
+
         Service.getApi(p)
             .then(response => {
                 console.log(response);
-                if (response.data.length > 0) {
-                    this.setState({ data: response.data });
+                if (response.status === 200) {
+                    this.setState({
+                        data: response.data
+                    });
                 }
-                this.setState({ loadComplete: true })
+                this.setState({
+                    loadComplete: true
+                })
+            }).then(response => {
+                console.log(this.state);
             })
             .catch(function (error) {
                 Swal.fire(error.toString())
                 console.log(error);
             });
+
     }
 
     delete = () => {
 
-        const { params } = this.props.match
+        const {
+            params
+        } = this.props.match
         const p = `/${params.id}`;
 
         Service.deleteApi(p)
@@ -49,8 +67,7 @@ class DeletePage extends Component {
                     }).then(() => {
                         this.props.history.push('/');
                     });
-                }
-                else {
+                } else {
                     Swal.fire({
                         icon: 'warning',
                         title: 'Something went wrong',
@@ -60,32 +77,31 @@ class DeletePage extends Component {
                         this.props.history.push('/');
                     });
                 }
-                }).catch((error) => {
-                    console.log("error-----------", error)
-                });
+            }).catch((error) => {
+                console.log("error-----------", error)
+            });
     }
 
     render() {
 
-        return (
-            <div className="container-fluid">
-                <Title title={'Delete'} />
-                <div className="card">
-                    <div className="card-header">
-                        <h2>Delete this element?</h2>
-                    </div>
-                    <div className="card-body">Content</div> 
-                    <div className="card-footer text-center">
-                        <Link to="/">
-                            <button type="button" className="btn btn-info mx-2">Go Back</button>
-                        </Link>   
-                        <button type="button" onClick={this.delete} className="btn btn-danger mx-2">Delete</button>
-                    </div>
-                </div>
+        return ( 
+            <div className = "container-fluid" >
+                <Title title = {'Delete'}/> 
+                <div className = "card">
+                    <div className = "card-header">
+                        <h2> Delete this element ? </h2> 
+                    </div> 
+                    <div className = "card-body"> Content </div>  
+                    <div className = "card-footer text-center">
+                        <Link to = "/">
+                            <button type = "button" className = "btn btn-info mx-2" > Go Back </button> 
+                        </Link>
+                    <button type = "button" onClick = {this.delete} className = "btn btn-danger mx-2" > Delete </button> 
+                    </div> 
+                </div> 
             </div>
         )
-      }
+    }
 }
-
 
 export default DeletePage;
