3c57ee985351bf103ec21707705a3a207bb4b381
alessandro <ale.pericolo@gmail.com>
2019-11-16 15:52:38 +0100

    handle link

--- src/components/common/HandleObject.js
+++ src/components/common/HandleObject.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { isWebUri } from 'valid-url';
 
 export default class HandleObject extends React.Component {
 
@@ -18,22 +19,49 @@ export default class HandleObject extends React.Component {
         }
     }
 
-    handleElement = (element, index) => {
+    handleElement = (element, key) => {
 
         if (element instanceof Object) {
-
             return (
                 <div>
+                    {/*
                     <button className="btn btn-primary btn-sm" type="button" data-toggle="collapse" data-target={`#collapse${this.props.data.id}`} aria-expanded="false" aria-controls="collapseExample">
                         Show
                     </button>
                     <div className="collapse mt-2" id={`collapse${this.props.data.id}`}>
                         {this.handleSubElement(element)}
                     </div>
+                    */}
+
+                    <button type="button" className="btn btn-primary btn-sm" data-toggle="modal" data-target={`#modal${this.props.data.id}`}>
+                        Show
+                    </button>
+
+                    <div className="modal fade" id={`modal${this.props.data.id}`} tabIndex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                        <div className="modal-dialog" role="document">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h5 className="modal-title" id="exampleModalLongTitle">{key}</h5>
+                                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div className="modal-body">
+                                    {this.handleSubElement(element)}
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-danger btn-sm" data-dismiss="modal">Close</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             )
-
         } else {
+
+            if ((/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/).test(element)) {
+                return <a className="btn btn-primary btn-sm" href={element} target="_blank" role="button">Link</a>
+            }
             return element
         }
 
@@ -50,7 +78,7 @@ export default class HandleObject extends React.Component {
                     <div className="row border-bottom">
                         <div className="col"><strong>{key}:</strong></div>
                         <div className="col">
-                            {this.handleElement(obj[key])}
+                            {this.handleElement(obj[key], key)}
                         </div>
                     </div>
                 </div>
