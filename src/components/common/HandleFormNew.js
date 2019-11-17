88e3ad39504b1c852b89836b75197672be060525
alessandro <ale.pericolo@gmail.com>
2019-11-17 18:45:49 +0100

    new car page

--- src/components/common/HandleFormNew.js
+++ src/components/common/HandleFormNew.js
@@ -1,34 +1,55 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import ArrayQuality from './ArrayQuality'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 export default class HandleFormNew extends React.Component {
 
-    state = {
-        qualityArray: [],
-        wiki: "",
-        qualityObject: { overall: "", mechanical: "", powertrain: "", body: "", interior: "", accessories: "" },
+    constructor(props) {
+        super(props);
+        this.state = { ...this.props.data };
+        //console.log(this.state)
     }
 
+
+    handleChange = (e) => {
+
+        const newState = { ...this.state }
+
+        if (["name", "rating"].includes(e.target.name)) {
+
+            let quality = { ...this.state.quality }
+            quality[e.target.id][e.target.name] = e.target.value
+        }
+        //static elements
+        else {
+            newState[e.target.name] = e.target.value;
+            this.setState(newState);
+        }
+        console.log(newState);
+    }
 
     addArrayQuality = (e) => {
         this.setState((prevState) => ({
-            qualityArray: [...prevState.qualityArray, { name: "", rating: "" }],
+            quality: [...prevState.quality, { name: "", rating: "" }],
         }));
     }
-    handleSubmit = (e) => { e.preventDefault() }
+
+    handleSubmit = (e) => { e.preventDefault(); console.log(this.state) }
 
     render() {
 
-        let { qualityArray } = this.state;
-        let { qualityObject } = this.state;
+        let { quality } = this.state;
 
         return (
-            <div>
+            <form onSubmit={this.handleSubmit} onChange={this.handleChange}>
                 <div className="form-group row">
                     <label htmlFor="manufacturer" className="col-md-4 col-sm-12 col-form-label col-form-label-sm text-md-right">
                         <strong>MANUFACTURER:</strong>
                     </label>
                     <div className="col-md-8 col-sm-12">
-                        <input type="text" name="manufacturer" id="manufacturer" className="form-control form-control-sm" />
+                        <input type="text" name="manufacturer" id="manufacturer" defaultValue={this.state.manufacturer} className="form-control form-control-sm" />
                     </div>
                 </div>
                 <div className="form-group row">
@@ -36,7 +57,7 @@ export default class HandleFormNew extends React.Component {
                         <strong>MODEL:</strong>
                     </label>
                     <div className="col-md-8 col-sm-12">
-                        <input type="text" name="model" id="model" className="form-control form-control-sm" />
+                        <input type="text" name="model" id="model" defaultValue={this.state.model} className="form-control form-control-sm" />
                     </div>
                 </div>
                 <div className="form-group row">
@@ -44,39 +65,31 @@ export default class HandleFormNew extends React.Component {
                         <strong>PRICE:</strong>
                     </label>
                     <div className="col-md-8 col-sm-12">
-                        <input type="number" name="price" id="price" className="form-control form-control-sm" />
+                        <input type="number" name="price" id="price" defaultValue={this.state.price} className="form-control form-control-sm" />
                     </div>
                 </div>
-                <div className="form-group col-12 text-center">
-                    <button className="btn btn-sm btn-light mx-2 my-3" onClick={this.addArrayQuality}> Add array quality</button>
-                    {
-                        qualityArray.map((obj, index) => {
-                            return (
-                                <div className="border border-light p-3">
-                                    <div key={index} className="form-group row">
-                                        <label htmlFor={`name${index}`} className="col-md-4 col-sm-12 col-form-label col-form-label-sm text-md-right">
-                                            <strong>{`NAME ${index + 1}:`}</strong>
-                                        </label>
-                                        <div className="col-md-8 col-sm-12">
-                                            <input type="text" name={`name${index}`} id={`name${index}`} className="form-control form-control-sm" />
-                                        </div>
-                                    </div>
-                                    <div key={index} className="form-group row">
-                                        <label htmlFor={`rating${index}`} className="col-md-4 col-sm-12 col-form-label col-form-label-sm text-md-right">
-                                            <strong>{`RATING ${index + 1}:`}</strong>
-                                        </label>
-                                        <div className="col-md-8 col-sm-12">
-                                            <input type="text" name={`rating${index}`} id={`rating${index}`} className="form-control form-control-sm" />
-                                        </div>
-                                    </div>
-                                </div>
-                            )
-                        })
-                    }
-
-
+                <div className="form-group row">
+                    <div className="col text-center">
+                        <button className="btn btn-sm btn-light mx-2 my-3" onClick={this.addArrayQuality}> Add quality</button>
+                        <ArrayQuality quality={quality} />
+                    </div>
                 </div>
-            </div>
+                <div className="form-group row">
+                    <label htmlFor="wiki" className="col-md-4 col-sm-12 col-form-label col-form-label-sm text-md-right">
+                        <strong>WIKI:</strong>
+                    </label>
+                    <div className="col-md-8 col-sm-12">
+                        <input type="text" name="wiki" id="wiki" defaultValue={this.state.wiki} className="form-control form-control-sm" />
+                    </div>
+                </div>
+
+                <div className="form-group col-12 text-center">
+                    <button type="submit" value="submit" className="btn btn-success mx-2" title="Save">
+                        <FontAwesomeIcon icon="save" size="lg" />
+                    </button>
+                </div>
+            </form>
+
         )
     }
 
--- src/components/container/NewPage.js
+++ src/components/container/NewPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// Service from '../api/Service';
+import Service from '../api/Service';
 //import Title from '../common/Title'
 import HandleFormNew from '../common/HandleFormNew'
 import { Link } from 'react-router-dom'
@@ -8,59 +8,63 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 class NewPage extends React.Component {
 
-  /*
   constructor(props) {
     super(props);
 
-    
     this.state = {
-        id: this.props.match.params.id,
-        data: '',
-        loadComplete: false
+      data: '',
+      loadComplete: false
     };
-    this.addService = new Service();
-    
   }
-  */
+
+  componentDidMount() {
+
+    this.setState({ loadComplete: true })
+    this.setState({
+      data: {
+        manufacturer: '',
+        model: '',
+        price: '',
+        quality: [],
+        wiki: '',
+      }
+    })
+  }
+
 
   save = () => {
     console.log('save');
+    console.log(this.state.data);
   }
 
   render() {
 
-    return (
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-10">
-            {/*<Title title={'New'} />*/}
-            {/*<form>*/}
-            <div className="card my-3">
-              <div className="card-header bg-dark text-success text-center">
-                <h2> New element </h2>
-              </div>
-              <div className="card-body">
-                <div className="container">
-                  <HandleFormNew />
+    if (this.state.loadComplete) {
+      return (
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-10">
+              {/*<Title title={'New'} />*/}
+              {/*<form>*/}
+              <div className="card my-3">
+                <div className="card-header bg-dark text-success text-center">
+                  <h2> New element </h2>
+                </div>
+                <div className="card-body">
+                  <div className="container">
+                    <HandleFormNew data={this.state.data} />
+                  </div>
                 </div>
               </div>
-              <div className="card-footer bg-dark text-center">
-                <Link to="/">
-                  <button type="button" className="btn btn-info mx-2" title="Go back">
-                    <FontAwesomeIcon icon="arrow-left" size="lg" />
-                  </button>
-                </Link>
-                <button onClick={this.save} /*type="submit"*/ className="btn btn-success mx-2" title="Save">
-                  <FontAwesomeIcon icon="save" size="lg" />
-                </button>
-              </div>
+              {/*</form>*/}
             </div>
-            {/*</form>*/}
           </div>
         </div>
-      </div>
-    )
+      )
+    } else {
+      return ''
+    }
   }
-}
 
+}
 export default NewPage;
